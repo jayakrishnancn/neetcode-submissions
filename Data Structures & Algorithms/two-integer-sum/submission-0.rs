55a0c23063b39d66025fcf59cd3aa9e1218312bf
Jayakrishnan C N <jayakrishnancn@gmail.com>
2026-04-16 22:44:34 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+
+        let mut two_sum_map = HashMap::new(); 
+
+        for (i, num) in nums.iter().enumerate() {
+            if let Some(&j) = two_sum_map.get(& (target - num)) {
+                return vec![j as i32, i as i32];
+            }
+            two_sum_map.insert(num, i);
+        }
+        vec![]
+
+    }
+}
